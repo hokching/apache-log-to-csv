fc0491ed3cff66d83beb2071bfe449861444879a
Per Bergland <per.bergland@meltwater.com>
2017-11-21 10:09:55 +0100

    Adding derived fields feature Progress now goes to stderr if stdout is used for output from tool

--- apache-log-to-csv.py
+++ apache-log-to-csv.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
+from __future__ import print_function  # Only needed for Python 2
 
 """
 Small tool to convert Apache log files to csv.
@@ -40,13 +41,21 @@ class Colors:
 
 def main(**kwargs):
 
-    #print('Converting, please wait...')
-
     line_parser = apache_log_parser.make_parser(kwargs['format'])
     header = True
     columns_arg = kwargs['columns']
     input_arg = kwargs['input']
     output_arg = kwargs['output']
+    derived_names = []
+    derived_blocks = []
+    derived_arg = kwargs['derivedcolumns']
+    if derived_arg:
+        derived_arg_split = derived_arg.split('|')
+        derived_names = derived_arg_split[0::2]
+        derived_blocks = derived_arg_split[1::2]
+        # TODO more validation of input here
+
+    progressFile = sys.stdout
 
     if input_arg == '-':
         inFile = sys.stdin
@@ -55,9 +64,11 @@ def main(**kwargs):
 
     if output_arg == '-':
         outFile = sys.stdout
+        progressFile = sys.stderr
     else:
         outFile = open(output_arg, 'w')
 
+    print('Converting, please wait...', file=progressFile)
 
     lines = inFile.readlines()
     writer = csv.writer(outFile, delimiter='\t')
@@ -66,8 +77,8 @@ def main(**kwargs):
         try:
             log_line_data = line_parser(line)
         except apache_log_parser.LineDoesntMatchException as ex:
-            print(Colors.FAIL + 'The format specified does not match the log file. Aborting...' + Colors.ENDC)
-            print('Line: ' + ex.log_line + 'RegEx: ' + ex.regex)
+            print(Colors.FAIL + 'The format specified does not match the log file. Aborting...' + Colors.ENDC, file=progressFile)
+            print('Line: ' + ex.log_line + 'RegEx: ' + ex.regex, file=progressFile)
             exit()
 
         if header:
@@ -75,15 +86,18 @@ def main(**kwargs):
                 columns = list(log_line_data.keys())
             else:
                 columns = columns_arg.split(',')
-            writer.writerow(columns)
+            writer.writerow(columns + derived_names)
             header = False
         else:
             values = []
             for column in columns:
                 values.append(log_line_data[column])
+
+            for derivedBlock in derived_blocks:
+                values.append(eval(derivedBlock))
             writer.writerow(values)
 
-    print(Colors.OKGREEN + 'Conversion finished.' + Colors.ENDC)
+    print(Colors.OKGREEN + 'Conversion finished.' + Colors.ENDC, file=progressFile)
 
 
 if __name__ == '__main__':
@@ -92,5 +106,6 @@ if __name__ == '__main__':
     parser.add_argument('--input', '-i', type=str, help='Input log file ex. /var/log/apache/access.log', default='-')
     parser.add_argument('--output', '-o', type=str, help='Output csv file ex. ~/accesslog.csv', default='-')
     parser.add_argument('--columns', '-c', type=str, help='Output columns, comma delimited, ex. status,time_received_utc_isoformat,request_url_query', default='ALL')
+    parser.add_argument('--derivedcolumns', '-d', type=str, help='Additional derived/calculated columns using python syntax, pipe delimited, ex. colname|log_line_data["request_url"].split("/")[-1]', default='')
     args = parser.parse_args()
     main(**vars(args))
